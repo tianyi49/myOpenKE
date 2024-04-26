e7debcef3e97ff01fed96def3fb9ef1983e99a60
tianyi49 <1154127892@qq.com>
2024-04-26 20:39:52 +0800

    删除标签和抽象等级数据，添加可逆关系等进行比较实验

--- test.py
+++ test.py
@@ -7,11 +7,15 @@ from openke.module.loss import MarginLoss
 from openke.module.strategy import NegativeSampling
 from openke.data import TrainDataLoader, TestDataLoader
 
+IN_PATH= "/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/after_process_invers_4_26/"
+
+
+
 def train_transe():
 
     # dataloader for training
     train_dataloader = TrainDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         nbatches=100,
         threads=8,
         sampling_mode="normal",
@@ -21,13 +25,13 @@ def train_transe():
         neg_rel=0)
 
     # dataloader for test
-    test_dataloader = TestDataLoader("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/", "link", False)
+    test_dataloader = TestDataLoader(IN_PATH, "link", False)
 
     # define the model
     transe = TransE(
         ent_tot=train_dataloader.get_ent_tot(),
         rel_tot=train_dataloader.get_rel_tot(),
-        dim=200,
+        dim=100,
         p_norm=1,
         norm_flag=True)
 
@@ -39,19 +43,19 @@ def train_transe():
     )
 
     # train the model
-    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=50,alpha=1.0, use_gpu=True)
+    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=100,alpha=1.0, use_gpu=True)
     trainer.run()
-    transe.save_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transe.ckpt')
+    transe.save_checkpoint(IN_PATH+'checkpoint/transe.ckpt')
 
     print("#####################TransE Test##########################")
     # test the model
-    transe.load_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transe.ckpt')
+    transe.load_checkpoint(IN_PATH+'checkpoint/transe.ckpt')
     tester = Tester(model=transe, data_loader=test_dataloader, use_gpu=True)
     tester.run_link_prediction(type_constrain=False)
 
 def   train_transh():
     train_dataloader = TrainDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         nbatches=100,
         threads=8,
         sampling_mode="normal",
@@ -61,35 +65,35 @@ def   train_transh():
         neg_rel=0)
 
     # dataloader for test
-    test_dataloader = TestDataLoader("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/", "link", False)
+    test_dataloader = TestDataLoader(IN_PATH, "link", False)
 
     # define the model
     transh = TransH(
         ent_tot=train_dataloader.get_ent_tot(),
         rel_tot=train_dataloader.get_rel_tot(),
-        dim=200,
+        dim=100,
         p_norm=1,
         norm_flag=True)
 
     # define the loss function
     model = NegativeSampling(
         model=transh,
-        loss=MarginLoss(margin=4.0),
+        loss=MarginLoss(margin=10.0),
         batch_size=train_dataloader.get_batch_size()
     )
 
     # train the model
-    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=1000, alpha=0.5, use_gpu=True)
+    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=100, alpha=0.5, use_gpu=True)
     trainer.run()
-    transh.save_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transh.ckpt')
+    transh.save_checkpoint(IN_PATH+'checkpoint/transh.ckpt')
     print("#####################TransH Test##########################")
     # test the model
-    transh.load_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transh.ckpt')
+    transh.load_checkpoint(IN_PATH+'checkpoint/transh.ckpt')
     tester = Tester(model=transh, data_loader=test_dataloader, use_gpu=True)
     tester.run_link_prediction(type_constrain=False)
 def train_transr():
     train_dataloader = TrainDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         nbatches=200,
         threads=8,
         sampling_mode="normal",
@@ -100,14 +104,14 @@ def train_transr():
 
     # dataloader for test
     test_dataloader = TestDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         sampling_mode='link',type_constrain = False)
 
     # define the model
     transe = TransE(
         ent_tot=train_dataloader.get_ent_tot(),
         rel_tot=train_dataloader.get_rel_tot(),
-        dim=200,
+        dim=100,
         p_norm=1,
         norm_flag=True)
 
@@ -119,8 +123,8 @@ def train_transr():
     transr = TransR(
         ent_tot=train_dataloader.get_ent_tot(),
         rel_tot=train_dataloader.get_rel_tot(),
-        dim_e=200,
-        dim_r=200,
+        dim_e=100,
+        dim_r=100,
         p_norm=1,
         norm_flag=True,
         rand_init=False)
@@ -135,22 +139,22 @@ def train_transr():
     trainer = Trainer(model=model_e, data_loader=train_dataloader, train_times=1, alpha=0.5, use_gpu=True)
     trainer.run()
     parameters = transe.get_parameters()
-    transe.save_parameters("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/result/transr_transe.json")
+    transe.save_parameters("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/wo_label_absn_4_26/result/transr_transe.json")
 
     # train transr
     transr.set_parameters(parameters)
     trainer = Trainer(model=model_r, data_loader=train_dataloader, train_times=100, alpha=1.0, use_gpu=True)
     trainer.run()
-    transr.save_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transr.ckpt')
+    transr.save_checkpoint(IN_PATH+'checkpoint/transr.ckpt')
     print("#####################TransR Test##########################")
     # test the model
-    transr.load_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/transr.ckpt')
+    transr.load_checkpoint(IN_PATH+'checkpoint/transr.ckpt')
     tester = Tester(model=transr, data_loader=test_dataloader, use_gpu=True)
     tester.run_link_prediction(type_constrain=False)
 def train_rescal():
     # dataloader for training
     train_dataloader = TrainDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         nbatches=100,
         threads=8,
         sampling_mode="normal",
@@ -161,7 +165,7 @@ def train_rescal():
     )
 
     # dataloader for test
-    test_dataloader = TestDataLoader("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/", "link", False)
+    test_dataloader = TestDataLoader(IN_PATH, "link", False)
 
     # define the model
     rescal = RESCAL(
@@ -181,17 +185,17 @@ def train_rescal():
     trainer = Trainer(model=model, data_loader=train_dataloader, train_times=400, alpha=0.1, use_gpu=True,
                       opt_method="adagrad")
     trainer.run()
-    rescal.save_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/rescal.ckpt')
+    rescal.save_checkpoint(IN_PATH+'checkpoint/rescal.ckpt')
     print("#####################Rescal Test##########################")
     # test the model
-    rescal.load_checkpoint('/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/checkpoint/rescal.ckpt')
+    rescal.load_checkpoint(IN_PATH+'checkpoint/rescal.ckpt')
     tester = Tester(model=rescal, data_loader=test_dataloader, use_gpu=True)
     tester.run_link_prediction(type_constrain=False)
 
 def train_transd():
     # dataloader for training
     train_dataloader = TrainDataLoader(
-        in_path="/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/",
+        in_path=IN_PATH,
         nbatches=100,
         threads=8,
         sampling_mode="normal",
@@ -201,7 +205,7 @@ def train_transd():
         neg_rel=0)
 
     # dataloader for test
-    test_dataloader = TestDataLoader("/home/tianyi/codeProjects/con_exploit/mydata/Openke_test/test_03_10/", "link", False)
+    test_dataloader = TestDataLoader(IN_PATH, "link", False)
 
     # define the model
     transd = TransD(
@@ -232,7 +236,7 @@ def train_transd():
 
 if __name__ == "__main__":
     train_transe()
-    train_transh()
+    # train_transh()
     # train_rescal()
     # train_transr()
     # train_transd()
